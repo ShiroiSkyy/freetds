0221625cb9ef6db94b8cefb82ef3f63996d76d0c
ShiroiSky <shiroisky@163.com>
2025-04-18 12:40:16 +0800

    enabled odbc wwide

---
+++ localcmake.bat
@@ -0,0 +1,37 @@
+curl https://codeload.github.com/win-iconv/win-iconv/zip/v0.0.8 -o win_iconv.zip
+7z x win_iconv.zip
+mv win-iconv-0.0.8 iconv
+wsl
+mkdir iconv-build
+exit
+cd iconv-build
+
+#cmake -G "NMake Makefiles" -DBUILD_STATIC=on -DBUILD_SHARED=off -DBUILD_EXECUTABLE=off -DBUILD_TEST=off -DCMAKE_BUILD_TYPE=Release ../iconv
+cmake -G "NMake Makefiles" -DBUILD_STATIC=on -DBUILD_SHARED=off -DBUILD_EXECUTABLE=off -DBUILD_TEST=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../iconv
+nmake
+
+cd ..
+
+wsl
+mkdir -p ./build/lib
+mkdir -p ./iconv/lib
+mkdir -p ./iconv/include
+
+cp ./iconv/iconv.h ./include/
+cp ./iconv/iconv.h ./iconv/include/
+cp ./iconv-build/iconv.lib ./iconv/lib/
+
+ls -la ./include/iconv.h
+ls -la ./iconv/include/iconv.h
+ls -la ./iconv/lib/iconv.lib
+exit
+
+cd build
+cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DENABLE_MSDBLIB=off -DENABLE_ODBC_WIDE=on -DBUILD_SHARED_LIBS=OFF -DWITH_OPENSSL=off -DCMAKE_INSTALL_PREFIX="E:\Github-Repositories\freetds\freetds_install" ..
+
+wsl
+grep -E "HAVE_ICONV|ICONV_CONST" ./include/config.h
+exit
+
+nmake
+nmake install
