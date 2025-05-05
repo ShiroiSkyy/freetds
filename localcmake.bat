5578fd125a7956a05dee63c334e8648ce54d3f5f
ShiroiSky <shiroisky@163.com>
2025-05-05 15:07:37 +0800

    添加DSN对ConnectTimeout的支持

--- localcmake.bat
+++ localcmake.bat
@@ -1,3 +1,5 @@
+@echo off
+echo "准备ICONV工作流"
 curl https://codeload.github.com/win-iconv/win-iconv/zip/v0.0.8 -o win_iconv.zip
 7z x win_iconv.zip
 mv win-iconv-0.0.8 iconv
@@ -6,12 +8,14 @@ mkdir iconv-build
 exit
 cd iconv-build
 
-#cmake -G "NMake Makefiles" -DBUILD_STATIC=on -DBUILD_SHARED=off -DBUILD_EXECUTABLE=off -DBUILD_TEST=off -DCMAKE_BUILD_TYPE=Release ../iconv
+#cmake -G "NMake Makefiles" -DBUILD_STATIC=on -DBUILD_SHARED=off -DBUILD_EXECUTABLE=off -DBUILD_TEST=off -DCMAKE_BUILD_TYPE=Release ../iconv、
+echo "CMake 配置 ICONV"
 cmake -G "NMake Makefiles" -DBUILD_STATIC=on -DBUILD_SHARED=off -DBUILD_EXECUTABLE=off -DBUILD_TEST=off -DCMAKE_BUILD_TYPE=Release -DCMAKE_POLICY_VERSION_MINIMUM=3.5 ../iconv
 nmake
 
 cd ..
 
+echo "准备FreeTDS工作流"
 wsl
 mkdir -p ./build/lib
 mkdir -p ./iconv/lib
@@ -27,12 +31,16 @@ ls -la ./iconv/lib/iconv.lib
 exit
 
 cd build
+echo "CMake 配置 FreeTDS"
 cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=Release -DENABLE_MSDBLIB=OFF -DENABLE_ODBC_WIDE=OFF -DBUILD_SHARED_LIBS=OFF -DWITH_OPENSSL=OFF -DCMAKE_INSTALL_PREFIX="E:\Github-Repositories\freetds\freetds_install" ..
 
 wsl
 grep -E "HAVE_ICONV|ICONV_CONST" ./include/config.h
 exit
 
+echo "开始编译 FreeTDS"
 nmake
+
+echo "安装 FreeTDS"
 nmake install
 
